8e68b0202dfdfbce2933f07cdc26808d6018b597
Imraan <imraan.mohamedhoesein@gmail.com>
2025-09-26 16:19:31 +0200

    gamegrid and leaderboard in components

---
+++ MiniGameVisual/Components/Elements/GameGrid.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MiniGameVisual.Components.Elements
+{
+  public partial class GameGrid
+  {
+    [Parameter] public char[,] Grid { get; set; } = new char[6, 5];
+    [Parameter] public string[,] GridStyles { get; set; } = new string[6, 5];
+
+    private string GetCellClass(int row, int col)
+    {
+      var style = GridStyles[row, col];
+      return style switch
+      {
+        "typed" => "cell-typed",
+        "correct" => "cell-correct",
+        "present" => "cell-present",
+        "absent" => "cell-absent",
+        _ => "cell-empty"
+      };
+    }
+  }
+}
---
+++ MiniGameVisual/Components/Elements/Leaderboard.razor.cs
@@ -0,0 +1,12 @@
+﻿using CodleLeaderboardDb.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace MiniGameVisual.Components.Elements
+{
+  public partial class Leaderboard
+  {
+    [Parameter] public List<LeaderboardEntry> LeaderboardList { get; set; } = [];
+    private static string FormatTime(TimeSpan time) =>
+    time.TotalSeconds < 60 ? $"{time.Seconds}s" : $"{(int)time.TotalMinutes}m {time.Seconds}s";
+  }
+}
